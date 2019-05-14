4b267a5fe241d162664b57112993730b54ba551f
mikestratton <mike@mikestratton.net>
2019-05-14 03:13:44 -0400

    travis-ci integration

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Sunnytree\\Acorn\\' => array($baseDir . '/src'),
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInite72a7a9f07f2e681277bbdc2e892793d
 {
+    public static $prefixLengthsPsr4 = array (
+        'S' => 
+        array (
+            'Sunnytree\\Acorn\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Sunnytree\\Acorn\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInite72a7a9f07f2e681277bbdc2e892793d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInite72a7a9f07f2e681277bbdc2e892793d::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
